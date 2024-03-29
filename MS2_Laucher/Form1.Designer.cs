d7d2c136d293a8186823c4f164919de87b294935
progeroshkg <vasakuzmenko552@gmail.com>
2024-03-29 22:12:04 +0600

    first commit

---
+++ MS2_Laucher/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace MS2_Laucher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startGameButton = new System.Windows.Forms.Button();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startGameButton
+            // 
+            this.startGameButton.Location = new System.Drawing.Point(12, 112);
+            this.startGameButton.Name = "startGameButton";
+            this.startGameButton.Size = new System.Drawing.Size(100, 30);
+            this.startGameButton.TabIndex = 2;
+            this.startGameButton.Text = "Start Game";
+            this.startGameButton.UseVisualStyleBackColor = true;
+            this.startGameButton.Click += new System.EventHandler(this.startGameButton_Click);
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.Location = new System.Drawing.Point(12, 180);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(100, 30);
+            this.settingsButton.TabIndex = 1;
+            this.settingsButton.Text = "Settings";
+            this.settingsButton.UseVisualStyleBackColor = true;
+            this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(12, 249);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(100, 30);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MS2_Laucher.Properties.Resources.spider_man_2_blog_1697207041401;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(787, 410);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.settingsButton);
+            this.Controls.Add(this.startGameButton);
+            this.Name = "Form1";
+            this.Text = "SM2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startGameButton;
+        private System.Windows.Forms.Button settingsButton;
+        private System.Windows.Forms.Button exitButton;
+
+        private void startGameButton_Click(object sender, EventArgs e)
+        {
+            // Код для обработки нажатия кнопки "Start Game"
+        }
+
+        private void settingsButton_Click(object sender, EventArgs e)
+        {
+            // Код для обработки нажатия кнопки "Settings"
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Код для обработки нажатия кнопки "Exit"
+            System.Windows.Forms.Application.Exit(); // Закрываем приложение
+        }
+    }
+}
